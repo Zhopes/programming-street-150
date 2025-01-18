96530ca9c76f422f8e0c73748429a7734fb6d298
Zhope <zhopessss@gmail.com>
2025-01-19 01:15:12 +0530

    add this one

---
+++ sprint2/sumOfFirstNOddNumber.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+// int main(){
+//   int n;
+//   cin>>n;
+//   int sum = 0;
+//   for(int i=1; i<=2*n; i++){
+//     if(i%2 == 1){
+//       sum += i;
+//     }
+//   }
+//   cout<<sum;
+//   return 0;
+// }
+
+int main(){
+  int n;
+  cin>>n;
+  int sum = n*n;
+  cout<<sum;
+  return 0;
+}
